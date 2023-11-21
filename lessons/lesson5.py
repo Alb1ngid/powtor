be6e03fee2de1ec6a5b47a467ba9c1b4ac74c1dc
Geek_Tech <geecktech.och2@gmail.com>
2023-11-21 16:43:35 +0600

    lass_5

--- lessons/lesson5.py
+++ lessons/lesson5.py
@@ -3,3 +3,32 @@
 # собвственные модули
 # внешние модули
 # venv виртуальная среда
+
+# множественное наследование
+# mro - порядок выполнения методов
+
+class A:
+    def a(self):
+        print('это метод класса А')
+
+class B:
+    def a(self):
+        print('это метод класса В')
+
+class C(A):
+    def a(self):
+        print('это метод С')
+
+
+class D(A,B,C):
+    ...
+
+c=D
+c.a(self=1)
+#1 murat,erbol
+print(D.mro())
+
+# def a()
+# a()
+# a()
+# lambda
